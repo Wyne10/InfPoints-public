782184d40b00f6fc93d4602862dd94becee18fd5
Wyne10 <izmodenov1997@gmail.com>
2025-03-14 21:43:41 +0500

    Ensure xp point singleton

--- src/main/java/org/bigcraft/infpoints/core/factory/MemoryPointFactory.java
+++ src/main/java/org/bigcraft/infpoints/core/factory/MemoryPointFactory.java
@@ -1,9 +1,11 @@
 package org.bigcraft.infpoints.core.factory;
 
+import com.google.inject.Singleton;
 import org.bigcraft.infpoints.core.MemoryPoint;
 import org.bigcraft.infpoints.core.Point;
 import org.bukkit.configuration.ConfigurationSection;
 
+@Singleton
 public class MemoryPointFactory implements PointFactory {
     @Override
     public Point create(ConfigurationSection config) {
--- src/main/java/org/bigcraft/infpoints/module/PointTypeModule.java
+++ src/main/java/org/bigcraft/infpoints/module/PointTypeModule.java
@@ -2,6 +2,7 @@ package org.bigcraft.infpoints.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.MapBinder;
+import org.bigcraft.infpoints.api.PointTypes;
 import org.bigcraft.infpoints.core.factory.*;
 
 public class PointTypeModule extends AbstractModule {
@@ -9,10 +10,10 @@ public class PointTypeModule extends AbstractModule {
     @Override
     protected void configure() {
         MapBinder<String, PointFactory> mapBinder = MapBinder.newMapBinder(binder(), String.class, PointFactory.class);
-        mapBinder.addBinding("MEMORY").to(MemoryPointFactory.class);
-        mapBinder.addBinding("JSON").to(JsonPointFactory.class);
-        mapBinder.addBinding("PDC").to(PdcPointFactory.class);
-        mapBinder.addBinding("XP").to(XpPointFactory.class);
+        mapBinder.addBinding(PointTypes.MEMORY.name()).to(MemoryPointFactory.class);
+        mapBinder.addBinding(PointTypes.JSON.name()).to(JsonPointFactory.class);
+        mapBinder.addBinding(PointTypes.PDC.name()).to(PdcPointFactory.class);
+        mapBinder.addBinding(PointTypes.XP.name()).to(XpPointFactory.class);
     }
 
 }
